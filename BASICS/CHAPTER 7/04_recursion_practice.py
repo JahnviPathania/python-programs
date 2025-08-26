df6ad6dc271e6287bac7e530c3e547ef7e1b4b66
Jahnvi Pathania <jahnvpathania3@gmail.com>
2025-08-26 20:04:58 +0530

    added one more example in recursion

--- BASICS/CHAPTER 7/04_recursion_practice.py
+++ BASICS/CHAPTER 7/04_recursion_practice.py
@@ -6,4 +6,20 @@ def factorial(n):
         fact=n*factorial(n-1)
         return fact
 print(factorial(5))
-    
+#write a recursive function to calculate the sum of first natural numbers
+def sum(n):
+    if(n==1):
+        return 1
+    else:
+        s=n+sum(n-1)
+        return s
+print(sum(5))
+#write a recursive function to print all elemenst in a list
+def print_elements(list,index):
+    if(index==len(list)):
+        return
+    print(list[index])
+    print_elements(list,index+1)
+
+list=[1,2,3,4,5]
+print_elements(list,0)
